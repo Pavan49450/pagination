59ee2dec02038d7c1033010e0c04a68f7f993375
pavan <pavan49450@gmail.com>
2023-08-12 13:35:39 +0530

    Setup project and Fetching API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useHttps from "./hooks/use-Https";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoading, sentRequest, error] = useHttps();
+  const [products, setProducts] = useState(null);
+
+  const fetchProducts = (data) => {
+    console.log("data->", data);
+    setProducts(data);
+    console.log("products->", products);
+  };
+
+  useEffect(() => {
+    sentRequest({ url: "https://dummyjson.com/products" }, fetchProducts);
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
